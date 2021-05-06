e4545f86cddd04d0695758bc6085557b7c7fe234
qing <997758846@qq.com>
2021-05-06 16:02:47 +0800

    Merge branch 'hox-fix'
    # Conflicts:
    #	src/main/java/Test01.java

--- src/main/java/Test01.java
+++ src/main/java/Test01.java
@@ -7,6 +7,7 @@ public class Test01 {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("git hox-fix");
+        System.out.println("git hox-fix");
 
     }
 }
